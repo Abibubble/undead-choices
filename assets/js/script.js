db6b53b1be99c0a2c0a52c0aa2a9688e94f220b3
Abibubble <abi.yamaha.neo@gmail.com>
2021-10-14 20:47:43 +0000

    #12 wip: Add auto updating copyright functionality

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// -------------------------------------------------------------------- Copyright
+
+function copyrightYear() {
+    var d = new Date();
+    var y = d.getFullYear();
+    document.getElementById("copyright").innerHTML = y;
+}
+
+copyrightYear();
