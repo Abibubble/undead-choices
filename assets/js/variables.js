a07ec332e79eff73ac7d23e9e72bcd1fdedf914b
DaveyJH <daveyj.dh@googlemail.com>
2021-10-17 00:17:59 +0100

    Add page control elements to variables

--- assets/js/variables.js
+++ assets/js/variables.js
@@ -5,3 +5,7 @@ let pronoun_they = "they";
 let zombieKingVisited = "kingNotSeen";
 let age = "child";
 let userName = "Zebedee";
+
+// ---------- page control
+const currentPage = document.getElementsByClassName("page current")[0];
+const waitingPage = document.getElementsByClassName("page waiting")[0];
