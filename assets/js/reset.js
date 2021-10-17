3e6716d29d8201072c52c6965fd98a98c8ba301e
Abibubble <abi.yamaha.neo@gmail.com>
2021-10-17 14:51:45 +0000

    #5 ref: Tidy code and comments

--- assets/js/reset.js
+++ assets/js/reset.js
@@ -1,10 +1,10 @@
-// ----------------------------- Reset all
+// ---------------------------------------------------------------- Reset all
 
 function reset() {
   pronoun_their = "their";
   pronoun_them = "them";
   pronoun_they = "they";
-  zombieKingVisited = "kingNotSeen"; //kingSeen
+  zombieKingVisited = "kingNotSeen"; // kingSeen
   age = "child";
   userName = "Zebedee";
   storyEnd = false;
