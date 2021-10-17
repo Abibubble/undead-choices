368b35dbecf38116696ab274087f6de4ba51f774
DaveyJH <daveyj.dh@googlemail.com>
2021-10-17 10:32:44 +0100

    DOC+ reset.js with reset function

---
+++ assets/js/reset.js
@@ -0,0 +1,16 @@
+// ----------------------------- Reset all
+
+function reset() {
+  pronoun_their = "their";
+  pronoun_them = "them";
+  pronoun_they = "they";
+  zombieKingVisited = "kingNotSeen"; //kingSeen
+  age = "child";
+  userName = "Zebedee";
+  storyEnd = false;
+  drinkColor = "";
+  hasDrink = false;
+  pageNumber = 0;
+  paraNumber = 1;
+  nextPage();
+}
